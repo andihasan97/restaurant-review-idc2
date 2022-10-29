d8d514623802f9a3ba5d843b0b14b93df039ea46
andihasan97 <andihasanelfalakiy@gmail.com>
2022-10-29 11:25:18 +0700

    codelab activity ktx

--- app/src/main/java/com/andihasan7/restaurantreviewidc2/MainActivity.kt
+++ app/src/main/java/com/andihasan7/restaurantreviewidc2/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
@@ -15,15 +16,18 @@ import com.google.android.material.snackbar.Snackbar
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val mainViewModel by viewModels<MainViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         supportActionBar?.hide()
 
-        val mainViewModel = ViewModelProvider(this, ViewModelProvider.NewInstanceFactory()).get(MainViewModel::class.java)
+        // val mainViewModel = ViewModelProvider(this, ViewModelProvider.NewInstanceFactory()).get(MainViewModel::class.java)
+
         mainViewModel.restaurant.observe(this, { restaurant ->
             setRestaurantData(restaurant)
         })
